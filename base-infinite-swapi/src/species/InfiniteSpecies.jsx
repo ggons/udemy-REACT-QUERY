8109bc9cfbcefbb8199f2f8b98b37fda443ee2c9
NamYoungGon <42319010+ggons@users.noreply.github.com>
2024-08-18 14:05:59 +0900

    end

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,13 +1,63 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Species } from "./Species";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Species } from './Species';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
-const initialUrl = "https://swapi.dev/api/species/";
+const initialUrl = 'https://swapi.dev/api/species/';
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery({
+    queryKey: ['species'],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || undefined;
+    },
+  });
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  if (isError) {
+    return <div className="error">Error... : {error.toString}</div>;
+  }
+
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll
+        initialLoad={false}
+        loadMore={() => {
+          if (!isFetching) {
+            fetchNextPage();
+          }
+        }}
+        hasMore={hasNextPage}
+      >
+        {data.pages.map((pageData) => {
+          return pageData.results.map((species) => {
+            return (
+              <Species
+                key={species.name}
+                name={species.name}
+                language={species.language}
+                averageLifespan={species.averageLifespan}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
